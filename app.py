267edd624c4b8e37f0081c2cb5d6af0474ddbdf1
harshinikruti <harshini.kruti.t@gmail.com>
2026-07-03 15:23:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import pandas as pd
+app = Flask(__name__)
+@app.route('/')
+def home():
+    df = pd.read_excel("movies.xlsx")
+    search = request.args.get('search')
+    if search:
+
+        df = df[df['Title'].astype(str).str.contains(            search,            case=False,
+            na=False        )]
+    movies = df.to_dict(orient='records')
+    total_movies = len(df)
+    return render_template(
+        "index.html",
+        movies=movies,
+        total_movies=total_movies    )
+if __name__ == '__main__':
+    app.run(debug=True)
